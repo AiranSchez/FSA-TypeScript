174d225f28819b01b8f8afeb45ee0959f53d544b
AiranSchez <airanschez0@gmail.com>
2021-02-01 14:28:03 +0000

    :tada: Ready to fight :tada:

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import { PersonalComputer } from './PersonalComputer';
+
+let ordenador: PersonalComputer = {
+    name: 'Predator',
+    brand: 'Acer',
+    price: 200
+}
+
+const sum = (num1: number, num2: number): number => {
+    return num1 + num2;
+}
+
+export default sum
